51d7b0597ae58c98100477e5a51673aec4d26c40
方凯kk <fkfkbill@gmail.com>
2013-06-04 09:20:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+        name = "djangomako" ,
+        version = "0.0.1" ,
+        description = "a simple django-mako connector for both py2 and py2." ,
+        author = "kK" ,
+        url = "http://kkblog.org" ,
+        license = "MIT" ,
+        packages = find_packages(),
+        scripts = [ " scripts/test.py " ],
+        )
