90f59bdc32a316e62e8e32c948dc0aa630f7558e
kk <fkfkbill@gmail.com>
2013-11-27 21:54:41 +0800

    fix

--- djangomako.py
+++ djangomako.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-__version__="0.2.1"
 '''
 django-mako template connection module
-version 0.2, for django1.5+,py3.3+
+version 0.1, for django1.5+
 
-author: kk(fkfkbill@gmail.com)
 '''
 #python import
 from glob import glob
@@ -16,53 +14,45 @@ from django.conf import settings
 
 #mako import
 from mako.lookup import TemplateLookup
-
-#default template lookup
 djlookup=TemplateLookup(directories=settings.TEMPLATE_DIRS,input_encoding="utf-8")
 
 
 
 def render_to_string(template_name,
-        dictionary=None,
-        request=None,
-        tmpldir=None):
-    '''
+		dictionary=None,
+		request=None):
+	'''
 render a template to a string(like render_to_string django.template.loader)
 '''
-    #if another template dir is specified,then use it.
-    if tmpldir==None or tmpldir=="":
-        t=djlookup.get_template(template_name)
-    else:
-        t=TemplateLookup(directories=tmpldir,input_encoding="utf-8").get_template(template_name)
-    if request!=None:
-        dictionary.update(csrf(request))
-    page=t.render(**dictionary)
-    return page
+	t=djlookup.get_template(template_name)
+	if request!=None:
+		dictionary.update(csrf(request))
+	page=t.render(**dictionary)
+	return page
 	
 
 
 def render_to_response(template_name,
-        dictionary={},
-        content_type="text/html",
-        request=None,
-        status=200,
-        tmpldir=None):
-    '''
+		dictionary={},
+		content_type="text/html",
+		request=None,
+		status=200):
+	'''
 a simple http response method just like django's
 for easy alternativity
 '''
-    page=render_to_string(template_name,dictionary,request,tmpldir=tmpldir)
-    return HttpResponse(content=page,content_type=content_type,status=status)
+	page=render_to_string(template_name,dictionary,request)
+	return HttpResponse(content=page,content_type=content_type,status=status)
 
 
 
 def tmpldebug(request,tmpl=""):
-    '''
+	'''
 argument:
 	tmpl:specific template file name.
 '''
-    if tmpl=="":
-        t='''
+	if tmpl=="":
+		t='''
 <!DOCTYPE html>
 <html>
 <head>
@@ -70,18 +60,20 @@ argument:
 </head>
 <body>
 '''
-        for tmpldir in settings.TEMPLATE_DIRS:
-            if tmpldir[-1]!="/":tmpldir+="/"
-            t+="<h2>"+tmpldir+"</h2>"
-            for s in glob(tmpldir+"*"):
-                if s[-1:]=="~":continue
-                s=s[len(tmpldir):]
-                t+='<p><a href=\"'+s+'\">'+s+"</a></p>"
-            t+="<br>"
-        t+='''
+		for tmpldir in settings.TEMPLATE_DIRS:
+			if tmpldir[-1]!="/":tmpldir+="/"
+			t+="<h2>"+tmpldir+"</h2>"
+			for s in glob(tmpldir+"*"):
+				if s[-1:]=="~":continue
+				s=s[len(tmpldir):]
+				t+='<p><a href=\"'+s+'\">'+s+"</a></p>"
+			s+="<br>"
+		t+='''
 </body> 
 </html>
 '''
-        return HttpResponse(t)
-    else:
-        return render_to_response(tmpl,{})
+		return HttpResponse(t)
+
+	else:
+		return render_to_response(tmpl,{})
+
